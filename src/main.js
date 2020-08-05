e4e57da064a5a1236c93c8d1ba36cb4b3a1bc573
418442040 <58377712+418442040@users.noreply.github.com>
2020-08-05 17:16:14 +0800

    整合element

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import App from './App.vue'
 //导入路由
 import router from './router/index.js'
 
+//导入element组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+//注册element
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
