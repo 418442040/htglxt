f957a3aa19de00edbacee692c2344281d8d396b2
418442040 <58377712+418442040@users.noreply.github.com>
2020-08-17 12:40:29 +0800

    路由嵌套整合

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,21 @@ import index from "../views/index/index.vue";
 //导入登录页
 import login from "../views/login/login.vue";
 
+//导入企业列表
+import enterprise from '../views/index/enterprise/enterprise.vue'
+
+//导入学科列表
+import subject from '../views/index/subject/subject.vue'
+
+//导入题库列表
+import question from '../views/index/question/question.vue'
+
+//导入用户列表
+import user from '../views/index/user/user.vue'
+
+//导入数据列表
+import chart from '../views/index/chart/chart.vue'
+
 //注册router
 Vue.use(VueRouter);
 
@@ -18,7 +33,34 @@ const router = new VueRouter ({
     routes: [
         {
             path: "/index",
-            component: index
+            component: index,
+            children: [
+                //企业列表
+                {
+                    path: 'enterprise',
+                    component: enterprise
+                },
+                //学科列表
+                {
+                    path: 'subject',
+                    component: subject
+                },
+                //用户列表
+                {
+                    path: 'user',
+                    component: user
+                },
+                //数据列表
+                {
+                    path:'chart',
+                    component: chart
+                },
+                //题库列表
+                {
+                    path:'question',
+                    component: question
+                }
+            ]
         },
         {
             path: "/login",
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,37 +1,81 @@
 <template>
   <div class="index_wrap">
+    <el-container>
+      <el-header class="my_header">
+        <!-- 头部左边的盒子 -->
+        <div class="my_header_left">
+          <i class="index_icon el-icon-s-fold" @click="collapse=!collapse"></i>
+          <img class="index_logo" src="../../assets/index_logo.png" alt="" />
+          <span class="index_title">黑马面面</span>
+        </div>
+        <!-- 头部右边的盒子 -->
+        <div class="my_header_right">
+          <img class="photo" src="../../assets/tx.png" alt="" />
+          <span class="index_name">李达,你好</span>
+          <el-button type="primary" size="mini" @click="userOut"
+            >退出</el-button
+          >
+        </div>
+      </el-header>
       <el-container>
-        <el-header class="my_header">
-          <!-- 头部左边的盒子 -->
-          <div class="my_header_left">
-            <i class="index_icon el-icon-s-fold"></i>
-            <img class="index_logo" src="../../assets/index_logo.png" alt="">
-            <span class="index_title">黑马面面</span>
-          </div>
-          <!-- 头部右边的盒子 -->
-          <div class="my_header_right">
-            <img class="photo" src="../../assets/tx.png" alt="">
-            <span class="index_name">李达,你好</span>
-            <el-button type="primary" size="mini">退出</el-button>
-          </div>
-        </el-header>
-        <el-container>
-          <el-aside width="200px" class="my_aside">Aside</el-aside>
-          <el-main class="my_main">Main</el-main>
-        </el-container>
+        <!-- 左侧导航栏 -->
+        <el-aside width="200px" class="my_aside">
+          <el-menu
+            :default-active="$route.path"
+            router
+            class="el-menu-vertical-demo"
+            :collapse="collapse"
+          >
+            <el-menu-item index="1">
+              <i class="el-icon-time"></i>
+              <span slot="title">数据概览</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-user"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-edit-outline"></i>
+              <span slot="title">题库列表</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-office-building"></i>
+              <span slot="title">企业列表</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+              <i class="el-icon-notebook-2"></i>
+              <span slot="title">学科列表</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <!-- 右侧功能模块 -->
+        <el-main class="my_main">
+          <router-view></router-view>
+        </el-main>
       </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      collapse:false
+    };
+  },
+  methods: {
+    userOut() {
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style lang="less">
 /* element布局容器样式 */
-html,body{
+html,
+body {
   height: 100%;
   padding: 0;
   margin: 0;
@@ -50,19 +94,19 @@ html,body{
     display: flex;
     align-items: center;
     .index_icon {
-    font-size: 30px;
-    margin-right: 22px;
-  }
-  .index_logo {
-    width: 28px;
-    height: 23px;
-    margin-right: 11px;
-  }
-  .index_title {
-    font-size: 22px;
-    font-weight: bold;
-    color: rgba(73,161,255,1);
-  }
+      font-size: 30px;
+      margin-right: 22px;
+    }
+    .index_logo {
+      width: 28px;
+      height: 23px;
+      margin-right: 11px;
+    }
+    .index_title {
+      font-size: 22px;
+      font-weight: bold;
+      color: rgba(73, 161, 255, 1);
+    }
   }
   .my_header_right {
     display: flex;
@@ -74,19 +118,17 @@ html,body{
     }
     .index_name {
       font-size: 14px;
-      color:rgba(99,99,99,1);
+      color: rgba(99, 99, 99, 1);
       margin-right: 38px;
-      font-weight:bold;
+      font-weight: bold;
     }
   }
 }
 .my_aside {
   height: 100%;
-  background-color:yellow;
 }
 .my_main {
   height: 100%;
   background-color: green;
 }
-
 </style>
